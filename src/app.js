514b9f4ccacefd50605313214b4740ace29aa9fd
Ivan Pedrazas <ipedrazas@gmail.com>
2015-07-13 19:02:32 +0100

    base64 lib added

--- src/app.js
+++ src/app.js
@@ -6,11 +6,16 @@
 
 var UI = require('ui');
 var Vector2 = require('vector2');
+var ajax = require('ajax');
+var Base64 = require('base64');
+
+// var TWILIO_URL = 'https://api.twilio.com/2010-04-01/Accounts/AC03e448fcd4b653801fb98dd24fc666e06/Messages.json';
+var test_url = 'http://api.theysaidso.com/qod.json';
 
 var main = new UI.Card({
-  title: 'Pebble.js',
+  title: 'OMW!',
   icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
+  subtitle: 'On my way...',
   body: 'Press any button.'
 });
 
@@ -20,9 +25,9 @@ main.on('click', 'up', function(e) {
   var menu = new UI.Menu({
     sections: [{
       items: [{
-        title: 'Pebble.js',
+        title: 'OMW',
         icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
+        subtitle: 'Let people know...'
       }, {
         title: 'Second Item',
         subtitle: 'Subtitle Text'
@@ -44,17 +49,39 @@ main.on('click', 'select', function(e) {
     position: new Vector2(0, 65),
     size: new Vector2(144, 30),
     font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
+    text: 'Press Select to send your OMW!',
     textAlign: 'center'
   });
   wind.add(textfield);
   wind.show();
-});
+  wind.on('click', 'select', function(e){
+    console.log('OMW sent');
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+    var credentials =  Base64.encode ("AC03e448fc4b653801fb98dd24fc666e06:49b3256910a46dea05f46f1f8ac8c65f");
+    console.logs(credentials);
+    
+//     var twilioBody = { 
+//         'to': "+447429346069", 
+//         'from': "+447903549460", 
+//         'body': "Pebble - Twilio Test",   
+//     };
+    
+    ajax(
+      {
+        url: test_url,
+        type: 'json',
+//         data: twilioBody,
+//         headers: {'Authorization': 'Basic ' + credentials} 
+      },
+      function(data) {
+        // Success!
+        console.log('Successfully fetched data!');
+        console.log(data);
+      },
+      function(error) {
+        // Failure!
+        console.log('Failed fetching data: ' + error);
+      }
+);
+  });
 });
